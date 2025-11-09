385c91d925ca0cfe1f290f8dc8ade34af1295112
VeeraMahendra7 <140475884+VeeraMahendra7@users.noreply.github.com>
2025-11-09 16:52:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
+let p = document.getElementById('ptag');
+let mainEle = document.getElementById('app');
+let btn = document.getElementById('swap');
+
+function swapTheme(){
+	mainEle.classList.add('night');
+	btn.classList.add('button_night');
+}
+
+btn.addEventListener('click', swapTheme)
 
